b74c909e7f0bf4d004cc7b1e2e67289f88f5fbe3
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2019-03-17 22:39:22 -0400

    finish component structure

--- src/app.js
+++ src/app.js
@@ -1,3 +1,16 @@
+class IndecisionApp extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Action />
+        <Options />
+        <AddOption />
+      </div>
+    )
+  }
+}
+
 class Header extends React.Component {
   render() {
     return (
@@ -23,7 +36,11 @@ class Options extends React.Component {
   render() {
     return (
       <div>
-        <p>Options</p>
+        <Option />
+        <Option />
+        <Option />
+        <Option />
+        <Option />
       </div>
     )
   }
@@ -39,13 +56,14 @@ class AddOption extends React.Component {
   }
 }
 
-const jsx = (
-  <div>
-    <Header />
-    <Action />
-    <Options />
-    <AddOption />
-  </div>
-)
+class Option extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Option</p>
+      </div>
+    )
+  }
+}
 
-ReactDOM.render(jsx, document.getElementById('app'))
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'))
