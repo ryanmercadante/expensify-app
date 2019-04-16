3542325fa68d53e11ec2fd669e11425586320955
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2019-04-16 19:19:14 -0400

    improved file structure and react-router implementation

--- src/app.js
+++ src/app.js
@@ -1,42 +1,8 @@
 import React, { Component } from 'react' 
 import ReactDOM from 'react-dom'
-import { BrowserRouter, Route } from 'react-router-dom'
+import AppRouter from './routers/AppRouter'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
-const ExpenseDashboardPage = () => (
-  <div>
-    This is from dashboard component
-  </div>
-)
 
-const AddExpensePage = () => (
-  <div>
-    This is add expense page
-  </div>
-)
-
-const EditExpensePage = () => (
-  <div>
-    This is the edit expense page
-  </div>
-)
-
-const HelpPage = () => (
-  <div>
-    This is the help page
-  </div>
-)
-
-const routes = (
-  <BrowserRouter>
-    <div>
-      <Route path='/' component={ ExpenseDashboardPage } exact={ true } />
-      <Route path='/create' component={ AddExpensePage } />
-      <Route path='/edit' component={ EditExpensePage } />
-      <Route path='/help' component={ HelpPage } />
-    </div>
-  </BrowserRouter>
-)
-
-ReactDOM.render(routes, document.getElementById('app'))
+ReactDOM.render(<AppRouter />, document.getElementById('app'))
