6c6f6c1a0ed54c54d895f82a81cac6072a83ecbf
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2019-04-18 11:35:24 -0400

    setup provider in root of app and create HOC using connect with ExpenseList

--- src/app.js
+++ src/app.js
@@ -1,22 +1,32 @@
-import React, { Component } from 'react' 
+import React from 'react' 
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 import AppRouter from './routers/AppRouter'
 import configureStore from './store/configureStore'
-import { addExpense, editExpense, removeExpense } from './actions/expenses'
+import { addExpense } from './actions/expenses'
 import { setTextFilter } from './actions/filters'
 import getVisibleExpenses from './selectors/expenses'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
 const store = configureStore()
-const { expenses, filters } = store.getState()
 
 store.dispatch(addExpense({ description: 'Water Bill', amount: 1000 }))
 store.dispatch(addExpense({ description: 'Gas Bill', amount: 3000 }))
-store.dispatch(setTextFilter('water'))
+store.dispatch(setTextFilter('bill'))
+
+setTimeout(() => {
+  store.dispatch(setTextFilter('water'))
+}, 3000)
 
 const state = store.getState()
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
 console.log(visibleExpenses)
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'))
+const jsx = (
+  <Provider store={store} >
+    <AppRouter />
+  </Provider>
+)
+
+ReactDOM.render(jsx, document.getElementById('app'))
