540472f33dbee034413c6562e514c1b2043d42a2
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2019-04-17 23:05:52 -0400

    create redux files

--- src/app.js
+++ src/app.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react' 
 import ReactDOM from 'react-dom'
 import AppRouter from './routers/AppRouter'
+import configureStore from './store/configureStore'
+import { addExpense, editExpense, removeExpense } from './actions/expenses'
+import { setTextFilter } from './actions/filters'
+import getVisibleExpenses from './selectors/expenses'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
+const store = configureStore()
+const { expenses, filters } = store.getState()
+
+store.dispatch(addExpense({ description: 'Water Bill', amount: 1000 }))
+store.dispatch(addExpense({ description: 'Gas Bill', amount: 3000 }))
+store.dispatch(setTextFilter('water'))
+
+const state = store.getState()
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
+console.log(visibleExpenses)
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'))
