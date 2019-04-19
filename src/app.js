4f9649cc2879d46b6210230dcb2f500e46332246
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2019-04-19 16:02:28 -0400

    create form to add expense

--- src/app.js
+++ src/app.js
@@ -12,8 +12,8 @@ import './styles/styles.scss'
 const store = configureStore()
 
 store.dispatch(addExpense({ description: 'Water Bill', amount: 1000 }))
-store.dispatch(addExpense({ description: 'Gas Bill', amount: 3000 }))
-store.dispatch(setTextFilter('bill'))
+store.dispatch(addExpense({ description: 'Gas Bill', createdAt: 3000 }))
+store.dispatch(addExpense({ description: 'Rent', amount: 109500 }))
 
 const state = store.getState()
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -1,12 +1,25 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { setTextFilter } from '../actions/filters';
+import { setTextFilter, sortByDate, sortByAmount } from '../actions/filters';
 
 const ExpenseListFilters = ({ filters, dispatch }) => (
   <div>
     <input type='text' value={filters.text} onChange={(e) => {
       dispatch(setTextFilter(e.target.value))
     }} />
+    <select 
+      value={filters.sortBy} 
+      onChange={(e) => {
+        if (e.target.value === 'date') {
+          dispatch(sortByDate())
+        } else if (e.target.value === 'amount') {
+          dispatch(sortByAmount())
+        }
+      }}
+    >
+      <option value='date'>Date</option>
+      <option value='amount'>Amount</option>
+    </select>
   </div>
 )
 
